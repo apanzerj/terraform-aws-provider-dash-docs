0d6e7ff2f0e4dd12864449658e0a923df24c5bed
Adam Panzer <adam.panzer@recurcrm.com>
2022-04-03 08:57:52 -0700

    Matrix Build: Two Versions (#1)
    * two versions
    
    * try again
    
    * Solve LaTex Issue

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
-wget https://github.com/hashicorp/terraform-provider-aws/archive/refs/heads/main.zip
-unzip main.zip
+wget https://github.com/hashicorp/terraform-provider-aws/archive/refs/tags/v$1.zip
+unzip -qq v$1.zip
 
-cp -r terraform-provider-aws-main/website/docs/r ./
-cp -r terraform-provider-aws-main/website/docs/d ./
-cp -r terraform-provider-aws-main/website/docs/index.html.markdown ./
+cp -r terraform-provider-aws-$1/website/docs/r ./
+cp -r terraform-provider-aws-$1/website/docs/d ./
+cp -r terraform-provider-aws-$1/website/docs/index.html.markdown ./
 
 echo "Creating resources.html.markdown"
 cat r/*.markdown > resources.html.markdown
@@ -15,11 +15,11 @@ sed -i'asdf' 's/```hcl/```terraform/gI' data_sources.html.markdown
 sed -i'asdf' 's/```hcl/```terraform/gI' resources.html.markdown
 
 echo "Converting resources.html.markdown to html"
-python -m gh_md_to_html -o OFFLINE resources.html.markdown
+python -m gh_md_to_html -o OFFLINE -m false resources.html.markdown
 echo "Converting data_sources.html.markdown to html"
-python -m gh_md_to_html -o OFFLINE data_sources.html.markdown
+python -m gh_md_to_html -o OFFLINE -f false data_sources.html.markdown
 echo "Converting index.html.markdown to html"
-python -m gh_md_to_html -o OFFLINE index.html.markdown
+python -m gh_md_to_html -o OFFLINE -m false index.html.markdown
 
 mv data_sources.html.html data_sources.html
 mv resources.html.html resources.html
@@ -33,10 +33,10 @@ sed -i'asdf' 's/\/github-markdown-css/github-markdown-css/g' index.html
 rm data_sources.html.markdown
 rm resources.html.markdown
 
-mkdir dash
-mv *.html dash/
-cp dashing.json dash/
-cp -r github-markdown-css dash/
+mkdir dash-$1
+mv *.html dash-$1/
+cp dashing.json dash-$1/
+cp -r github-markdown-css dash-$1/
 
 rm *.*asdf
 echo "Done"
